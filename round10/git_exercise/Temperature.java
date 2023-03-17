a5ef7c47d91a343dbcd853ee5841f27e1dafee84
Elias Nikkinen <elias.nikkinen@tuni.fi>
2023-03-17 14:43:33 +0200

    part1

--- round10/git_exercise/Temperature.java
+++ round10/git_exercise/Temperature.java
@@ -8,7 +8,10 @@ public class Temperature {
         try {
             System.out.print("Input temperature: ");
             double inputTemp = Double.parseDouble(scanner.nextLine());
-            System.out.printf("%.2f C = %.2f F\n", inputTemp, tTransform(inputTemp, 'c'));
+            System.out.printf("%.2f C = %.2f F\n",
+                    inputTemp, tTransform(inputTemp, 'c'));
+            System.out.printf("%.2f F = %.2f C\n",
+                    inputTemp, tTransform(inputTemp, 'f'));
         } catch (NumberFormatException e) {
             System.out.println("Not valid input");
             System.exit(1);
@@ -18,6 +21,9 @@ public class Temperature {
     public static double tTransform(double temp, char type) {
         if (type == 'c') {
             return temp * 9/15 + 32;
+        }
+        if (type == 'f') {
+            return (temp - 32) * 15/9;
         }
         return Double.NaN;
     }
